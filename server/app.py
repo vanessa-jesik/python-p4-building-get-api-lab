5fd4091320128067037cec35439f3faa373c8f68
Vanessa Jesik <vajesik@gmail.com>
2023-10-09 18:31:04 -0600

    completed lab

--- server/app.py
+++ server/app.py
@@ -1,38 +1,65 @@
 #!/usr/bin/env python3
 
-from flask import Flask, make_response, jsonify
+from flask import Flask, jsonify, make_response
 from flask_migrate import Migrate
-
-from models import db, Bakery, BakedGood
+from models import BakedGood, Bakery, db
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.json.compact = False
 
 migrate = Migrate(app, db)
 
 db.init_app(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return '<h1>Bakery GET API</h1>'
+    return "<h1>Bakery GET API</h1>"
 
-@app.route('/bakeries')
+
+@app.route("/bakeries")
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    bakeries_serialized = [bakery.to_dict() for bakery in bakeries]
 
-@app.route('/bakeries/<int:id>')
+    response = make_response(jsonify(bakeries_serialized), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/bakeries/<int:id>")
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first()
+    bakery_serialized = bakery.to_dict()
 
-@app.route('/baked_goods/by_price')
+    response = make_response(jsonify(bakery_serialized), 200)
+    response.headers["Content-Type"] = "application/json"
+    return response
+
+
+@app.route("/baked_goods/by_price")
 def baked_goods_by_price():
-    return ''
+    baked_goods_by_price = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    baked_goods_serialized = [
+        baked_good.to_dict() for baked_good in baked_goods_by_price
+    ]
+    response = make_response(jsonify(baked_goods_serialized), 200)
+    response.headers["Content-Type"] = "application/json"
 
-@app.route('/baked_goods/most_expensive')
+    return response
+
+
+@app.route("/baked_goods/most_expensive")
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    most_expensive_serialized = most_expensive.to_dict()
+    response = make_response(jsonify(most_expensive_serialized), 200)
+    response.headers["Content-Type"] = "application/json"
 
-if __name__ == '__main__':
+    return response
+
+
+if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,20 +1,46 @@
+from datetime import datetime
+
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+metadata = MetaData(
+    naming_convention={
+        "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    }
+)
 
 db = SQLAlchemy(metadata=metadata)
 
+
 class Bakery(db.Model, SerializerMixin):
-    __tablename__ = 'bakeries'
+    __tablename__ = "bakeries"
+
+    serialize_rules = ("-baked_goods.bakery",)
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    baked_goods = db.relationship("BakedGood", backref="bakery")
+
+    def __repr__(self):
+        return f"<Bakery {self.name}>"
+
 
 class BakedGood(db.Model, SerializerMixin):
-    __tablename__ = 'baked_goods'
+    __tablename__ = "baked_goods"
+
+    serialize_rules = ("-bakery.baked_goods",)
 
     id = db.Column(db.Integer, primary_key=True)
-    
+    name = db.Column(db.String)
+    price = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    bakery_id = db.Column(db.Integer, db.ForeignKey("bakeries.id"))
+
+    def __repr__(self):
+        return f"<Baked Good {self.name}, ${self.price}>"
